8131e26f552cd1998ed937046c3dd668fde655fe
amir nachum <amir.nachum.41@gmail.com>
2019-12-23 09:57:07 +0200

    venv

--- JackTokenizer.py
+++ JackTokenizer.py
@@ -25,14 +25,19 @@ NULL = "null"
 THIS = "this"
 
 class JackTokenizer:
-    def __init__(self, input_file):
-        self.input_file = input_file
+    def __init__(self, input_file_path):
+        self.input_file = open(input_file_path, "r")
+
 
     def hasMoreTokens(self):
+
         return True
 
     def advance(self):
-        return true
+        if not self.next_word:
+            return self.next_word
+        #if there are no more words, return None
+        return None
 
     def keyWord(self):
         return True
