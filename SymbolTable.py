c77fd6d947f9944bb3dde8eda12d4207e68021d1
amir nachum <amir.nachum.41@gmail.com>
2020-01-14 20:04:41 +0200

    Symbol Table

--- SymbolTable.py
+++ SymbolTable.py
@@ -20,33 +20,74 @@ class SymbolTable:
         :param name: name of the variable
         :param type: the type of the variable e.g int, boolean, class_name, etc
         :param kind: STATIC, FIELD, ARG, VAR
-        :return:
+        :return: True if succeeded in defining, false otherwise
         """
+
+        if name in [self.subroutine_table.keys(), self.class_level_table.keys()]:
+            # this is not a new variable
+            return True
+
+        # this is a new variable
+        index = self.var_count(kind) + 1
         if kind in CLASS_SCOPE:
-            self.class_level_table[name] =
+            self.class_level_table[name] = [index, kind, type]
+            return True
+        elif kind in SUBROUTINE_SCOPE:
+            self.subroutine_table[name] = [index, kind, type]
+            return True
+        return False
 
     def var_count(self, kind):
         """
         :param kind: static, field, arg, var
-        :return: num of variables of this kind
+        :return: int - num of variables of this kind
         """
-
-
+        if kind in CLASS_SCOPE:
+            #we substract 1, because the indexes are saved from 0 and not 1
+            return len([item for item in self.class_level_table.values() if item[1] == kind]) - 1
+        elif kind in SUBROUTINE_SCOPE:
+            # we substract 1, because the indexes are saved from 0 and not 1
+            return len([item for item in self.subroutine_table.values() if item[1] == kind]) - 1
+        return 0
 
     def kind_of(self, name):
         """
         :param name: string - name of variable
         :return: the variable kine e.g static, field, arg or var
         """
+        try:
+            return self.class_level_table[name][1]
+        except:
+            try:
+                return self.subroutine_table[name][1]
+            except:
+                #the given variable does not exist
+                return None
 
     def type_of(self, name):
         """
         :param name: string - the name of the variable
         :return: the type of the variable e.g (int, boolean, class_name)
         """
+        try:
+            return self.class_level_table[name][2]
+        except:
+            try:
+                return self.subroutine_table[name][2]
+            except:
+                #the given variable does not exist
+                return None
 
     def index_of(self, name):
         """
         :param name: string - the name of the variable
         :return: int - the index in the appropriate stack
         """
+        try:
+            return self.class_level_table[name][0]
+        except:
+            try:
+                return self.subroutine_table[name][0]
+            except:
+                #the given variable does not exist
+                return None
--- tests.py
+++ tests.py
@@ -1,12 +1,28 @@
-import JackTokenizer
+# import JackTokenizer
+#
+# tokenizer = JackTokenizer.JackTokenizer("text.txt")
+# # string = ""
+# # while(tokenizer.hasMoreTokens()):
+# #     tmp = tokenizer.advance()
+# #     string += tmp
+# #     print(tmp)
+#
+# funcput = "out\"comment\"out\"comment\"out\"comment\""
+# for val in tokenizer.maybe_smart_split(funcput):
+#     print(val)
 
-tokenizer = JackTokenizer.JackTokenizer("text.txt")
-# string = ""
-# while(tokenizer.hasMoreTokens()):
-#     tmp = tokenizer.advance()
-#     string += tmp
-#     print(tmp)
+import SymbolTable
 
-funcput = "out\"comment\"out\"comment\"out\"comment\""
-for val in tokenizer.maybe_smart_split(funcput):
-    print(val)
+table = SymbolTable.SymbolTable()
+table.start_subroutine()
+table.define('x', 'int', SymbolTable.VAR)
+table.define('y', 'int', SymbolTable.VAR)
+print(table.index_of('x'))
+print(table.type_of('x'))
+print(table.kind_of('x'))
+print(table.index_of('y'))
+table.start_subroutine()
+table.define('x', 'boolean', SymbolTable.VAR)
+print(table.index_of('x'))
+print(table.type_of('x'))
+print(table.kind_of('x'))
